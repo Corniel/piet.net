0c08211e4252ad21029b162c27d6b524a82f4ddc
Corniel Nobel <mail@corniel.nl>
2019-07-04 22:23:07 +0200

    Added Pi calculation.

---
+++ test/PietDotNet.UnitTests/Pi052Test.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using PietDotNet.Tests.Tooling;
+
+namespace PietDotNet.Tests
+{
+    public class Pi052Test : ProgramTestBase
+    {
+        protected override string Location => "pi_052.png";
+
+        [Test]
+        public void Run_PiWithRIs52Px_ProducesHelloWorld()
+        {
+            Interpreter.Run();
+
+            Assert.AreEqual(new long[] { 31_405 }, IO.OutInts);
+        }
+    }
+}
