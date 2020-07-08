1833cc5bcd7ec4234f8c0481983d1e911dba8696
Corniel Nobel <mail@corniel.nl>
2020-07-08 11:03:15 +0200

    entry point.

---
+++ src/PietDotNet/Compilation/Files/Piet.cs
@@ -0,0 +1,18 @@
+﻿using PietDotNet;
+using PietDotNet.IO;
+using PietDotNet.Logging;
+
+namespace Piet
+{
+    internal class Program
+    {
+        private static readonly InOut ConsoleIO = new ConsoleIO();
+
+        public static void Main()
+        {
+            var program = Binary.Load(typeof(Program).Assembly.GetManifestResourceStream("binary.piet"));
+            var logger = new ConsoleLogger(Microsoft.Extensions.Logging.LogLevel.Error);
+            program.Run(ConsoleIO, logger);
+        }
+    }
+}
---
+++ src/PietDotNet/Compilation/PietCompilation.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System;
+
+namespace PietDotNet.Compilation
+{
+    public static class PietCompilation
+    {
+        public static CSharpCompilation Create(Program program, string name)
+        {
+            Guard.NotNull(program, nameof(program));
+            Guard.NotNullOrEmpty(name, nameof(name));
+
+            var compilation = CSharpCompilation.Create
+            (
+                assemblyName: name,
+                syntaxTrees: Array.Empty<CSharpSyntaxTree>(),
+                references: new [] 
+                {
+                    MetadataReference.CreateFromFile(typeof(object).Assembly.Location)
+                },
+               options: new CSharpCompilationOptions(
+                   outputKind: OutputKind.ConsoleApplication, 
+                   optimizationLevel: OptimizationLevel.Release)
+            );
+
+            return compilation;
+        }
+    }
+}
